fb292127ced785fd558a26ec478e3eab85ced0d0
Rolando Velasco <rvelasco@fullsail.edu>
2013-03-21 18:37:51 -0700

    completed the code for clearing local storage, editing and deleting a goal.  Modified additem and css stylesheet.

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ window.addEventListener("DOMContentLoaded", function() {
 		var myformTag = document.getElementsByTagName("form"), selectList = document.getElementById("selecttype"), createSelect = document.createElement("select");
 		createSelect.setAttribute("id", "types");
 	
-		for (var i = 0, j = goalTypes.length; i < j; i++) {
+		for(var i = 0, j = goalTypes.length; i < j; i++) {
 			var createOption = document.createElement("option");
 			var optionText = goalTypes[i];
 			createOption.setAttribute("value", optionText);
@@ -29,10 +29,16 @@ window.addEventListener("DOMContentLoaded", function() {
 		}
 	}
 	
-	function addNewGoal() {
+	function addNewGoal(key) {
 	
-		// random number generator
-		var uniqueID = Math.floor(Math.random() * 1000001);
+	// Check if a key was passed.  A key will come from editGoal.
+	// Saving a new goal will send no key, but create a randomly generated key
+	if(!key) {
+			// random number generator
+			var uniqueID = Math.floor(Math.random() * 1000001);
+	} else {
+		uniqueID = key;
+	}
 		
 		// Find out if Goal Complete checkbox is checked or not
 		getGoalCompleteCheckBoxValue();
@@ -50,8 +56,7 @@ window.addEventListener("DOMContentLoaded", function() {
 		// Save the data into local Storage
 		// Use Stringify to convert the object into a string
 		localStorage.setItem(uniqueID, JSON.stringify(goalObj));
-		alert("Goal Saved!");	
-		return;
+		alert("Goal Saved!");
 	}
 	
 	function toggleDisplay() {
@@ -62,7 +67,6 @@ window.addEventListener("DOMContentLoaded", function() {
 			document.getElementById("display").style.display = "inline";
 			document.getElementById("add").style.display = "none";
 			document.getElementById("goals").style.display = "none";
-
 		} else {
 			flagDisplayData = true;
 			document.getElementById("goalform").style.display = "none";
@@ -70,8 +74,6 @@ window.addEventListener("DOMContentLoaded", function() {
 			document.getElementById("display").style.display = "none";
 			document.getElementById("add").style.display = "inline";
 		}
-		
-		return;
 	}
 	
 	// Grab the data from Local Storage and display it on screen
@@ -82,39 +84,44 @@ window.addEventListener("DOMContentLoaded", function() {
 		
 		if (localStorage.length === 0) {
 			alert("There are no goals to display.");
-			
+			return false;
 		}
 		
-		// Create a div tag
-		var createDisplayGoalsDiv = document.createElement("div");
-		createDisplayGoalsDiv.setAttribute("id", "goals");
-		var createUList = document.createElement("ul");
-		createDisplayGoalsDiv.appendChild(createUList);
-		document.body.appendChild(createDisplayGoalsDiv);
-		document.getElementById("goals").style.display = "block";
-		for (var i = 0, j = localStorage.length; i < j; i++) {
-			var createListItem = document.createElement("li");
-			var createLinksList = document.createElement("li");
-			createUList.appendChild(createListItem);
-			var key = localStorage.key(i);
-			var value = localStorage.getItem(key);
+		// Create a div tag IF one has not been created.
+		// If the list has been created, skip this block of code and only display the current list
+		if(document.getElementById("goals") === null) {
+			var createDisplayGoalsDiv = document.createElement("div");
+			createDisplayGoalsDiv.setAttribute("id", "goals");
+			var createUList = document.createElement("ul");
+			createDisplayGoalsDiv.appendChild(createUList);
+			document.body.appendChild(createDisplayGoalsDiv);
+			document.getElementById("goals").style.display = "block";
+			for (var i = 0, j = localStorage.length; i < j; i++) {
+				var createListItem = document.createElement("li");
+				var createLinksList = document.createElement("li");
+				createUList.appendChild(createListItem);
+				var key = localStorage.key(i);
+				var value = localStorage.getItem(key);
+				
+				// "parse" converts string value from Local Storage back into an object
+				var goalObj = JSON.parse(value);
 			
-			// "parse" converts string value from Local Storage back into an object
-			var goalObj = JSON.parse(value);
-		
-			var createSubUList = document.createElement("ul");
-			createListItem.appendChild(createSubUList);
-			
-			for(var n in goalObj) {
-				var createSubListItem = document.createElement("li");
-				createSubUList.appendChild(createSubListItem);
-				var goalObjText = goalObj[n][0] + " " + goalObj[n][1];
-				createSubListItem.innerHTML = goalObjText;
-				createSubUList.appendChild(createLinksList);
+				var createSubUList = document.createElement("ul");
+				createListItem.appendChild(createSubUList);
+				
+				for(var n in goalObj) {
+					var createSubListItem = document.createElement("li");
+					createSubUList.appendChild(createSubListItem);
+					var goalObjText = goalObj[n][0] + " " + goalObj[n][1];
+					createSubListItem.innerHTML = goalObjText;
+					createSubUList.appendChild(createLinksList);
+				}
+				
+				// calling the function that will create links for each goal
+				createGoalLinks(localStorage.key(i), createLinksList);
 			}
-			
-			// calling the function that will create links for each goal
-			createGoalLinks(key, createLinksList);
+		} else {
+			document.getElementById("goals").style.display = "block";
 		}
 	}
 	
@@ -137,7 +144,7 @@ window.addEventListener("DOMContentLoaded", function() {
 		deleteGoalLink.key = key;
 		deleteGoalLink.setAttribute("class", "goallinks");
 		deleteGoalText = "Delete Goal";
-//		deleteGoalLink.addEventListener("click", deleteGoal);
+		deleteGoalLink.addEventListener("click", deleteGoal);
 		deleteGoalLink.innerHTML = deleteGoalText;
 		createLinksList.appendChild(deleteGoalLink);
 	
@@ -145,6 +152,8 @@ window.addEventListener("DOMContentLoaded", function() {
 	
 	// function to edit the goal
 	function editGoal() {
+
+	
 		// grab the data from this specific goal from Local Storage
 		var value = localStorage.getItem(this.key);
 		var goalObj = JSON.parse(value);
@@ -170,7 +179,21 @@ window.addEventListener("DOMContentLoaded", function() {
 		var editSaveGoal = document.getElementById("savegoal");
 		
 		editSaveGoal.addEventListener("click", validate);
-		editSaveGoal.key = key;
+		editSaveGoal.key = this.key;
+	}
+	
+	function deleteGoal() {
+		var ask = confirm("Are you sure you want to delete this goal?");
+		if (ask) {
+			localStorage.removeItem(this.key);
+			alert("Goal was deleted.");
+			window.location.reload();
+		} else {
+			alert("Goal was not deleted.");
+		}
+	
+	
+	
 	}
 	
 	function clearGoalsList() {
@@ -184,10 +207,20 @@ window.addEventListener("DOMContentLoaded", function() {
 		}
 	}
 	
-	function validate() {
+	function validate(eventData) {
 		// elements to check
 		var getGoalHeadline = document.getElementById("goalheadline");
 		var getGoalType = document.getElementById("types");
+		
+		// Clear any current error messages
+		errorMessages.innerHTML = "";
+		
+		// Reset borders
+		getGoalHeadline.style.border = "1px solid black";
+		getGoalType.style.border = "1px solid black";
+
+		
+		
 		
 		// error messages
 		var errorMessageArray = [];
@@ -195,7 +228,7 @@ window.addEventListener("DOMContentLoaded", function() {
 		// goal headline validation
 		if(getGoalHeadline.value === "") {
 			var goalHeadlineError = "Please enter a goal.";
-			goalHeadline.style.border = "1px solid red";
+			getGoalHeadline.style.border = "1px solid red";
 			errorMessageArray.push(goalHeadlineError);
 		}
 		
@@ -211,14 +244,19 @@ window.addEventListener("DOMContentLoaded", function() {
 			for(var i = 0, j = errorMessageArray.length; i < j; i++) {
 				var text = document.createElement("li");
 				text.innerHTML = errorMessageArray[i];
-			
+				errorMessages.appendChild(text);
 			}
-	
+			eventData.preventDefault();
+			return false;
+		} else {
+			addNewGoal(this.key);
+		}
 	}
 	
 	// array for the goal types
 	var goalTypes = ["--Choose a Type--", "Personal", "Business", "Travel", "Finance", "Education"];
 	var goalCompleteValue = "Goal Not Yet Achieved";
+	errorMessages = document.getElementById("errors");
 	makeGoalTypes();
 	
 	// toggle variable for checking when to hide the form to display the data or vice versa
